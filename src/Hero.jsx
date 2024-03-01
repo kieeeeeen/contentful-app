58fd6241225efc4654f1216299fed83ddfbce9e3
kieeeeeen <nguyentrugkien111120@gmail.com>
2024-03-01 21:01:24 +0900

    first commit

---
+++ src/Hero.jsx
@@ -0,0 +1,18 @@
+import heroImg from "./assets/hero.svg";
+
+const Hero = () => {
+  return (
+    <section className="hero">
+      <div className="hero-center">
+        <div className="hero-title">
+          <h1>Contentful CMS</h1>
+          <p>Some small projects</p>
+        </div>
+        <div className="img-container">
+          <img src={heroImg} alt="woman and the browser" className="img" />
+        </div>
+      </div>
+    </section>
+  );
+};
+export default Hero;
